26a08e0a147dfe7f052e94e7f8274f13e7065bbd
Dimka <141173731+Dmitr1yy@users.noreply.github.com>
2023-09-02 18:50:02 +0400

    Update get-email-domain.js

--- get-email-domain.js
+++ get-email-domain.js
@@ -1,4 +1,4 @@
-asdfasdfsasdfasdfssadfasdfsafsмичсм12312чсмячмячzcxvzxcvzxvzxcvzxcvzxcvasdfasdfasdfdfaadfconst { NotImplementedError } = require('../extensions/index.js');
+asdfasdfasdfasdfsasdfasdfssadfasdfsafsмичсм12312чсмячмячzcxvzxcvzxvzxcvzxcvzxcvasdfasdfasdfdfaadfconst { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Given an email address, return it's domain.sadfasdfs
