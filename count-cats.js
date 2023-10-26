13d67915862e82ca84c3286ddfb3b5280fed1d1e
Dimka <141173731+Dmitr1yy@users.noreply.github.com>
2023-10-26 19:50:25 +0300

    Update count-cats.js

--- count-cats.js
+++ count-cats.js
@@ -15,8 +15,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function countCats(matrix) {
-   let count = 0;
-   for (let arr of matrix) {
+asdfasdf   for (let arr of matrix) {
       if (arr == '^^') {
          count++;
       }
